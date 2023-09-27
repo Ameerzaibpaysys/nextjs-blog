354dfe301a769a8b0987243a13f2eee5768c315b
ameerzaibduet <126883138+ameerzaibduet@users.noreply.github.com>
2023-09-27 20:48:50 +0500

    picture changed

--- src/components/banner/LeftBanner.js
+++ src/components/banner/LeftBanner.js
@@ -4,11 +4,8 @@ function LeftBanner() {
   <div className='relative flex justify-center'>
   <div className='flex justify-center items-center '>
         <img src="/Images/MyImg.png" alt="bannerImg" 
-    className='w-[500px] h-[600px z-10'
+    className='w-[500px] h-[600px z-10 opacity-60'
     /></div>
-<div className='absolute bottom-0 w-[500px] h-[500px] bg-gradient-to-r from-[#1e2024] to-[#202327] shadow-shadowOne flex justify-center items-center ml-1/2'>
-    
-</div>
   </div>  
   )
 }
