0ba72ef605494001f54194f82e823c349e907294
Ameerzaibpaysys <156815823+Ameerzaibpaysys@users.noreply.github.com>
2024-02-22 19:23:12 +0500

    Update Project.js

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -4,35 +4,35 @@ import ProjectCard from './ProjectCard'
 
 const Project = () => {
   return (
-    <section id='projects' 
-    className='w-full py-10 border-b-[1px] border-b-black'>
-       <div className='flex justify-center items-center text-center'>
-       <Title
-        heading="VISIT MY PORTFOLIO"
-        dec="My Projects"
-        /> 
-        </div> 
-        <div className='grid grid-cols-3 gap-14'>
+    <section id='projects'
+      className='w-full py-10 border-b-[1px] border-b-black'>
+      <div className='flex justify-center items-center text-center'>
+        <Title
+          heading="VISIT MY PORTFOLIO"
+          dec="My Projects"
+        />
+      </div>
+      <div className='grid grid-cols-3 gap-14'>
         <ProjectCard
-        title="SOCIAL MEDIA CLONE"
-        des="It is a long established fact that a reader will be distracted by the readable content of a page 
+          title="SOCIAL MEDIA CLONE"
+          des="It is a long established fact that a reader will be distracted by the readable content of a page 
              when "
-              src="/Images/socialMedia.webp"
+          src="/Images/socialMedia.webp"
         />
-         <ProjectCard
-        title="E-COMMERCE WEBSITE"
-        des="It is a long established fact that a reader will be distracted by the readable content of a page 
+        <ProjectCard
+          title="E-COMMERCE WEBSITE"
+          des="It is a long established fact that a reader will be distracted by the readable content of a page 
             "
-             src="/Images/eCommerce.png"
+          src="/Images/eCommerce.png"
         />
-         <ProjectCard
-        title="CHATTING APP"
-        des="It is a long established fact that a reader will be distracted by the readable content of a page 
+        <ProjectCard
+          title="CHATTING APP"
+          des="It is a long established fact that a reader will be distracted by the readable content of a page 
              when"
-             src="/Images/chattingApp.png"
+          src="/Images/chattingApp.png"
         />
-        </div>
-        
+      </div>
+
     </section>
   )
 }
